bcc466996f008b88047f89a6795a93eeb842ce88
norikenpi <norikenpi@gmail.com>
2025-02-07 23:34:20 +0900

    change service name

--- initial_setup.sh
+++ initial_setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo ln -s $(pwd)/bin/network_check.sh /usr/local/bin/network_check
-sudo ln -s $(pwd)/systemd/network_check.service /etc/systemd/system/network_check.service
+sudo ln -s $(pwd)/bin/network_monitor.sh /usr/local/bin/network_monitor
+sudo ln -s $(pwd)/systemd/network_monitor.service /etc/systemd/system/network_monitor.service
 sudo systemctl daemon-reload
-sudo systemctl enable network_check.service
+sudo systemctl enable network_monitor.service
 
